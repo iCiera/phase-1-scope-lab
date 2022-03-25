e9966388ebb5bd48b76a5325cda04e16c51ae97e
ciera <cieranicole.m15@gmail.com>
2022-03-25 18:43:54 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'
+
+var upperCaseCustomerName = () => customerName = 'BOB'
+ 
+var setBestCustomer = () => bestCustomer = 'not bob'
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+
+const leastFavoriteCustomer = 'plankton'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'squidward'
+}
